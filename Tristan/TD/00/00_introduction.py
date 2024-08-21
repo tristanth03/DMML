193a5066368244023bbd9febb09fb11bb4c21804
tristanth03 <tristanthordarson@gmail.com>
2024-08-21 13:56:28 +0000

    uppfl

--- Tristan/TD/00/00_introduction.py
+++ Tristan/TD/00/00_introduction.py
@@ -14,18 +14,37 @@ def plot_normal(sigma: np.float64, mu:np.float64, x_start: np.float64, x_end: np
     n = 500
     x = np.linspace(x_start,x_end,n)
     y = normal(x,sigma,mu)
-    p = plt.plot(x,y)
+    p = plt.plot(x,y,label=fr'$\sigma$ = {sigma}, $\mu$ = {mu}')
     return p
 
     
-# def _plot_three_normals():
-#     # Part 1.2
+def _plot_three_normals():
+    # Part 1.2
+    plot_normal(0.5,0,-5,5)
+    plot_normal(0.25,1,-5,5)
+    plot_normal(1,1.5,-5,5)
+    plt.legend(loc='upper left')
+    
+    
+    
 
-# def normal_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
-#     # Part 2.1
+def normal_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
+    # Part 2.1
+    p = 0
+    for i in range(0,len(sigmas)):
+        a = weights[i]/np.sqrt(2*np.pi*(sigmas[i]**2))
+        b = np.exp(-np.power((x-mus[i]),2)/(2*(sigmas[i]**2)))
+        p += a*b
+    return p
 
-# def _compare_components_and_mixture():
-#     # Part 2.2
+def _compare_components_and_mixture():
+    # Part 2.2
+    _plot_three_normals()
+    x_ = np.linspace(-5,5,500)
+    p_multi = normal_mixture(x_,[0.5,1.5,0.25],[0,-0.5,1.5],[1/3,1/3,1/3])
+    plt.plot(x_,p_multi,label="mix")
+    plt.legend(loc='upper left')
+    plt.show()
 
 # def sample_gaussian_mixture(sigmas: list, mus: list, weights: list, n_samples: int = 500):
 #     # Part 3.1
@@ -33,5 +52,7 @@ def plot_normal(sigma: np.float64, mu:np.float64, x_start: np.float64, x_end: np
 # def _plot_mixture_and_samples():
 #     # Part 3.2
 
-# if __name__ == '__main__':
-#     # select your function to test here and do `python3 template.py
+if __name__ == '__main__':
+    _compare_components_and_mixture()
+
+    
